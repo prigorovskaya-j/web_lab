cd359e490f6a2130a8f96f9a31d705b37f32f549
Julia <prigorovskaya.j.i.2.18@gmail.com>
2020-12-19 10:35:30 +0300

    lab two - без проверки ФИО и вопроса

--- JS/index.js
+++ JS/index.js
@@ -17,3 +17,22 @@ function outputInteres(id, title, text, image, name_image, ...params){
      
         document.write(`</ul></p></div></div>`);     
 }
+function outputPhotos() {
+    let foto = [];
+    let title = [];
+    const n = 15;
+ 
+    for (let i = 0; i < n; i++) {
+        foto[i] = "images/"+(i + 1) + ".jpg";
+    }
+ 
+    for (let i = 0; i < n; i++) {
+        title[i] = "Рисунок" + (i + 1);
+    }
+ 
+    for (let i = 0; i < n; i++) {
+        document.write(`<figure><img title="Рисунок ` + (i + 1) + `" src="images/` + (i + 1) + `.jpg">
+    <figcaption>Рисунок ${i+1}</figcaption></figure>`);
+    }
+}
+ 
